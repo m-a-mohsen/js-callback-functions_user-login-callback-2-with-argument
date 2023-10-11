d03799749eba90e2773a36dbcad6266d70367625
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-11 16:20:44 +0200

    Fixed the build.

--- js/index.js
+++ js/index.js
@@ -4,4 +4,14 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe");
 }
 
-// The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName} You are logged in now.`);
+}
+
+
+
+handleUserLogin(showWelcomeMessage);
+handleUserLogin((userName) =>
+  console.log(`Welcome ${userName} You are logged in now.`)
+);
